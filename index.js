b42181dd2e2cb3f9acd21480e5aeaa3e3e0b992a
PanZheng <panzheng@admaster.com.cn>
2013-08-09 01:07:01 +0800

    changed style

--- index.js
+++ index.js
@@ -3,15 +3,19 @@
 	var body = $("body");
 	var getList = function(){
 			$.ajax({
-			url:"http://www.zhiyao99.com/pc",
+			url:"http://1.93.12.109:8080/pc",
 			type:"get",
 			dataType:"jsonp",
 			jsonp:"jsoncallback",
 			data:{method:"list",offset:offset},
 			success:function(d){
+				console.log(d);
 				var list = d.list;
 				offset = d.offset;
-				list.forEach(function(o){});
+
+				list.forEach(function(o){
+					body.append
+				});
 			}
 		});
 	}
