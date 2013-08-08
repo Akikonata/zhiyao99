1cb11d6ddc576ddb8916846dfba974a014e031fb
PanZheng <panzheng@admaster.com.cn>
2013-08-09 00:43:13 +0800

    added style

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 (function(){
-	
+	var offset = null;
+	var body = $("body");
+	var getList = function(){
+			$.ajax({
+			url:"http://www.zhiyao99.com/pc",
+			type:"get",
+			dataType:"jsonp",
+			jsonp:"jsoncallback",
+			data:{method:"list",offset:offset},
+			success:function(d){
+				var list = d.list;
+				offset = d.offset;
+				list.forEach(function(o){});
+			}
+		});
+	}
+	getList();
 })();
